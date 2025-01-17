75bb1c0e05dafea0d9c7c0e925eb58b902ebec7a
Raghuveer Chauhan <raghuveerchauhan7787@gmail.com>
2025-01-17 17:21:07 +0530

    adding only one  origin allow

--- src/app.js
+++ src/app.js
@@ -1,26 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-const allowedOrigins = ['http://localhost:3000', 'https://devtinder-web-8dlo.onrender.com'];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://devtinder-web-8dlo.onrender.com",
+];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (e.g., mobile apps, Postman)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true); // Origin is allowed
-    } else {
-      callback(new Error('Not allowed by CORS')); // Origin is not allowed
-    }
-  },
-};
-app.use(cors(corsOptions));
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -43,12 +38,12 @@ connectDB()
     });
   })
   .catch((err) => {
-    console.error("Database cannot be connected!!",err);
+    console.error("Database cannot be connected!!", err);
     console.log("Error in Database connections");
   });
 
-  // mongodb+srv://raghuveerchauhan7787:<db_password>@namastenodejs.88ivh.mongodb.net/?retryWrites=true&w=majority&appName=namasteNodejs
+// mongodb+srv://raghuveerchauhan7787:<db_password>@namastenodejs.88ivh.mongodb.net/?retryWrites=true&w=majority&appName=namasteNodejs
 
-  // Raghuveer__M001M
+// Raghuveer__M001M
 
-  // node src/app.js
+// node src/app.js
